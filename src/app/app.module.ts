93bf031c5e577051a001f7d372bee9d8234b3fc9
Dennis Tsiang <dennis.tsiang15@imperial.ac.uk>
2018-12-17 14:13:11 +0000

    Set up the homepage with a dropzone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { UploaderComponent } from './uploader/uploader.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploaderComponent
   ],
   imports: [
     BrowserModule
