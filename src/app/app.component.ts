983fd07ae98e94496a832513633279ccdd5ee84c
Dennis Tsiang <dennis.tsiang15@imperial.ac.uk>
2019-03-28 12:57:09 +0000

    Add total energy and colour code rating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,9 @@ export class AppComponent {
   public showData = false;
   public hardwareData: GraphData = null;
   public apiData: GraphData = null;
+  public truncatedTotalEnergy: string = null;
   public rating: string = null;
+  public ratingClass: string = null;
 
   public onData(energyDataResponse: EnergyDataResponse): void {
     console.log("Received energy data response");
@@ -24,9 +26,13 @@ export class AppComponent {
       .map(dataPair => dataPair[1]);
     let apiLabels = energyDataResponse.apiData.map(dataPair => dataPair[0]);
     let apiValues = energyDataResponse.apiData.map(dataPair => dataPair[1]);
+    let totalEnergy = hardwareValues.reduce((x,y) => x+y) +
+      apiValues.reduce((x,y) => x+y);
+    this.truncatedTotalEnergy = totalEnergy.toFixed(7);
     this.hardwareData = { labels: hardwareLabels, values: hardwareValues };
     this.apiData = { labels: apiLabels, values: apiValues };
     this.rating = energyDataResponse.rating;
+    this.ratingClass = "rating-" + this.rating;
     this.showData = true;
     setTimeout(() => document.getElementById("rating")
       .scrollIntoView({behavior: 'smooth', block: "end"})
